0c1cfa8738780c76989c73a693a489d7ab919faf
Wasswa Enock Male <wasswaenockmale@gmail.com>
2022-04-12 10:01:34 +0300

    Looping through numbers

---
+++ loops/counting.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+# This program counts numbers from 1 to 10.
+# It counts by looping for a fixed number of times.
+
+for i in 1 2 3 4 5 6 7 8 9 10
+do 
+	echo -n "...$i"
+done
+
+echo # This cleans up the next shell

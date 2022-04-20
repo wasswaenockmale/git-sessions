1d6678965fa2fc808fe2688c1db41396a7ceadf4
Wasswa Enock Male <wasswaenockmale@gmail.com>
2022-04-20 18:15:07 +0300

    Added in another way to write the text entry dialog

--- zenity/dialog/text_entry_dialog.sh
+++ zenity/dialog/text_entry_dialog.sh
@@ -12,3 +12,10 @@ fi
 
 
 # echo "$ENTRY"
+# It can also be done like this.
+# ENTRY=zenity --entry \
+#	--text="Commit message" \
+#	--text="Enter the commit message for last change." \
+#	--entry-text="Enter message"
+
+

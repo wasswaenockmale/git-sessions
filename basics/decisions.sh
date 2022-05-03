0d69341de9d912d065cae37fcf272b5a50175c29
Wasswa Enock Male <wasswaenockmale@gmail.com>
2022-05-03 13:08:43 +0300

    Created a .sh.swp file after running the .sh file

--- basics/decisions.sh
+++ basics/decisions.sh
@@ -16,3 +16,8 @@ case $choice in
 		echo "This is not a real product, you know."
 		;;
 esac
+echo "I have to make the work do some thing for me and then we have to make the world a better place for everyone and now that we have to make the world a better place and then we have to make it happen for everyone and then call me blessed for i am blessed for real right now i feel the blessing flowing over me right now. Thanks for considering me and looking up to me for the world is getting closer to something that i want to make my best part please consider quite people the best quiet people. Always never quit the "
+echo "Quitters never win. Winners never quit"
+echo "Simple people for real make life easy for everyone in the world."
+echo "Zero is not a number always remember that."
+echo "Zero is always part of big values. Don't you think that is every important."
